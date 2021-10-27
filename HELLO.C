984c1414d70c16471e7d6eea62ca7846388d63d4
Karthikeyan1710 <93249391+Karthikeyan1710@users.noreply.github.com>
2021-10-27 15:02:17 +0530

    Add files via upload

---
+++ HELLO.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//variable decleration
+int karthi=0;
+//******************************Main**************************
+void main()
+{
+clrscr();
+	printf("hello world");
+	printf("%d",karthi);
+getch();
+}
+//*****************************End****************************
